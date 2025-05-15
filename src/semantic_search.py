60da29b6c72823f93ab1b1d154225f855d3f0bde
Areesha Tahir <82057205+Areesha-Tahir@users.noreply.github.com>
2025-05-15 17:24:21 +0500

    pausing fine tuning

--- src/semantic_search.py
+++ src/semantic_search.py
@@ -4,7 +4,7 @@ import utils
 print("Initializing semantic search...")
 db_data = utils.db_connection_and_data()
 processed_data, titles = utils.data_preprocessing(db_data)
-utils.fine_tuning(processed_data)
+#utils.fine_tuning(processed_data)
 model_path = utils.upzip_saved_model()
 model = utils.load_saved_model(model_path)
 index = utils.faiss_index(model, processed_data)
